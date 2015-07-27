d2f5d3b7d2739cf982cb378f02fc6a59e7d94b92
Matthew Provis <matt@mattprovis.com>
2015-07-27 20:02:04 +1000

    Added the proper generic type to the instance of AllOf matcher in FluentMatcher base class

--- fluentmatcher-maven-plugin/src/main/java/com/mattprovis/fluentmatcher/FluentMatcher.java
+++ fluentmatcher-maven-plugin/src/main/java/com/mattprovis/fluentmatcher/FluentMatcher.java
@@ -1,16 +1,16 @@
 package com.mattprovis.fluentmatcher;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.hamcrest.Description;
 import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeDiagnosingMatcher;
 import org.hamcrest.core.AllOf;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 public abstract class FluentMatcher<BeanType> extends TypeSafeDiagnosingMatcher<BeanType> {
     protected Map<String, FeatureMatcher<BeanType, ?>> matchers = new TreeMap<>();
@@ -27,8 +27,8 @@ public abstract class FluentMatcher<BeanType> extends TypeSafeDiagnosingMatcher<
         return allMatchers().matches(item, mismatchDescription);
     }
 
-    private AllOf<?> allMatchers() {
-        List<Matcher<?>> allMatchers = new ArrayList<>();
+    private AllOf<BeanType> allMatchers() {
+        List<Matcher<? super BeanType>> allMatchers = new ArrayList<>();
         for (FeatureMatcher<BeanType, ?> fieldMatcher : matchers.values()) {
             allMatchers.add(fieldMatcher);
         }
